5dd8f2ff0ba907ced04aa1d06afd34cb75ee55be
JoséFilho <josefilhoredesocial@gmail.com>
2024-05-20 00:26:35 -0300

    em processo

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ function showSlider(type) {
         list.insertBefore(items[items.length -1], items[0]);
     }
 }
+
+
+/*document.addEventListener('scroll', function() {
+    let lateral = document.querySelector('.lateral');
+    let footer = document.querySelector('.content-footer');
+
+    let lateralRect = lateral.getBoundingClientRect();
+    let footerRect = footer.getBoundingClientRect();
+
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    let lateralTop = scrollTop + lateralRect.top;
+    let footerTop = scrollTop + footerRect.top;
+
+    if (scrollTop > lateralTop - 10) {
+        if (footerRect.top <= lateralRect.height + 10) {
+            lateral.classList.remove('fixed');
+            lateral.classList.add('stop');
+            lateral.style.top = (footerTop - lateralRect.height - 10) + 'px';
+        } else {
+            lateral.classList.remove('stop');
+            lateral.classList.add('fixed');
+            lateral.style.top = '40px';
+        }
+    } else {
+        lateral.classList.remove('fixed');
+        lateral.classList.remove('stop');
+        lateral.style.top = '100vh'; // Define a posição inicial da lateral
+    }
+});*/
+
+
